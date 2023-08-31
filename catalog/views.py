1a87e194c89de9f86aed3e0649a350397b344b59
Grandic <d.bitkov@gmail.com>
2023-08-31 21:07:06 +0300

    Add main page+

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'catalog/index.html')
+
+def index2(request):
+    return render(request, 'catalog/index2.html')
