1e73228a4fb8eae686c7035b655590a0dfc11f29
Grandic <d.bitkov@gmail.com>
2023-09-12 19:18:57 +0300

    All were other

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -2,3 +2,16 @@ from django import template
 
 register = template.Library()
 
+
+@register.filter(name='mediapath')
+def mediapath(value):
+    if value:
+        return f'/media/{value}'
+    return '#'
+
+
+@register.simple_tag
+def mediapath(val):
+    if val:
+        return f'/media/{val}'
+    return '#'
